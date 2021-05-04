db121b3c8d6011b0d429b78b9d51549d6e63dcc5
Joe Himes <joehimes42@gmail.com>
2021-05-04 16:14:49 -0400

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f426a65c945719eb10dea0cc22c0d7ce&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather services.', undefined);
+        } else if (body.error) {
+            callback(body.error.info, undefined);
+        } else {
+            const temperature = body.current.temperature;
+            const feelsLike = body.current.feelslike;
+            const sky = body.current.weather_descriptions[0];
+            callback(undefined, `${sky}. It is currently ${temperature} degrees. It feels like ${feelsLike} degrees.`)
+        }
+    })
+}
+
+module.exports = forecast;
